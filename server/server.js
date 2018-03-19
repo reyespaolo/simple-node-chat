3a88579b503051432e160b2bba9fd145b648d709
Paolo Reyes <reyes.paolo@360logix.com>
2018-03-19 18:08:59 +0800

    Added Websocket on connect and on disconnect

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) =>{
+  console.log('New User Connected');
+
+  socket.on("disconnect", () => {
+      console.log("User was Disconnected..");
+  });
+
+});
+
+server.listen(port, () => {
   console.log(`Server is up at port ${port}`);
 })
